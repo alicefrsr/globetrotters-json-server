91bd3ea0c7ea7bdfe2a6a2d82626deb780f5590d
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2023-10-13 19:06:20 +0200

    port 5001, changing script to node server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./data/cities.json');
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 3000;
+const PORT = 5001;
 
 server.use(middlewares);
 server.use(router);
